46c4384bae34f01290e9ee2002bdf656f5722dce
GoodKid <GoodKid88@yandex.ru>
2022-05-21 12:28:43 +0300

    add option: write player's name

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
-import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws FileNotFoundException {
-        Human player = new Human("Andrei");
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter you name: ");
+        Human player = new Human(scanner.nextLine());
         Computer computer = new Computer();
         Game game = new Game(player, computer);
         game.startGame();
